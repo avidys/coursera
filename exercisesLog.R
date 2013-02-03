623be539696f97e1d3ca5ffa3246fa9d54bfb00a
Jean <jm@avidys.com>
2013-02-03 13:26:11 -0800

    laptop

---
+++ exercisesLog.R
@@ -0,0 +1,116 @@
+#
+######  W1
+#
+
+heights = rnorm(10,mean=188,sd=3)
+heights
+
+args(rbinom)
+coinFlips = rbinom(10,size=10,prob=0.5)
+coinFlips
+
+args(dnorm)
+x = seq(from=-5,to=5,length=10)
+normalDensity = dnorm(x,mean=0,sd=1)
+normalDensity
+round(normalDensity,2)
+
+args(dbinom)
+x = seq(0,10,by=1)
+binomialDensity = dbinom(x,size=10,prob=0.5)
+round(binomialDensity,2)
+
+heights = rnorm(10,mean=188,sd=3)
+round(heights,2)
+sample(heights,size=10,replace=TRUE)
+sample(heights,size=5,replace=FALSE)
+
+set.seed(12345)
+rnorm(5,mean=0,sd=1)
+
+#
+######  W2
+#
+
+library(kernlab)
+data(spam)
+dim(spam)
+
+#setwd("./data")
+getwd()
+setwd("./data")
+setwd("../")
+
+#download.file("https://data.baltimorecity.gov/Transportation/Baltimore-Fixed-Speed-Cameras/dz54-2aru")
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl,destfile="./data/cameras.csv") #,method="curl")
+list.files("./data")
+dateDownloaded <- date()
+dateDownloaded
+
+cameraData <- read.table("./data/cameras.csv")
+head(cameraData)
+cameraData <- read.table("./data/cameras.csv",sep=",",header=TRUE)
+head(cameraData)
+
+library(xlsx)
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
+download.file(fileUrl,destfile="./data/camera.xlsx",method="curl")
+cameraData <- read.xlsx2("./data/camera.xlsx",sheetIndex=1)
+head(cameraData)
+
+file.choose()
+
+list.files("./data")
+
+#Important parameters file, header, sep, row.names, nrows
+#Related: read.csv(), read.csv2()
+#read.table()
+cameraData <- read.table("./data/cameras.csv")
+head(cameraData)
+getwd()
+cameraData <- read.table("./data/cameras.csv",sep=",",header=TRUE)
+head(cameraData)
+cameraData <- read.csv("./data/cameras.csv")
+
+#read.xlsx(), read.xlsx2() {xlsx package}
+#Reads .xlsx files, but slow
+#Important parameters file, sheetIndex, sheetIndex, rowIndex, colIndex, header
+#read.xlsx2() relies more on low level Java functions so may be a bit faster
+
+library(xlsx)
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
+download.file(fileUrl,destfile="./data/camera.xlsx",method="curl")
+cameraData <- read.xlsx2("./data/camera.xlsx",sheetIndex=1)
+head(cameraData)
+
+cameraData <- read.csv(file.choose())
+
+library(kernlab)
+data (spam)
+dim(spam)
+n=dim(spam)[1]
+n
+
+# training set
+set.seed(3435)
+trainI=rbinom(n,size=1,prob=.5)
+length(trainI)
+table(trainI)
+trainSpam=spam[trainI==1,]
+trainHam=spam[trainI==0,]
+dim(trainHam)
+dim(trainSpam)
+dimnames(trainHam[1,])
+names(trainSpam)
+head(trainSpam)
+table(trainSpam$type)
+length(trainSpam$type)
+plot(trainSpam$capitalAve ~ trainSpam$type)
+plot(log10(trainSpam$capitalAve+1) ~ trainSpam$type)
+plot(log10(trainSpam[,1:4]+1))
+hC=hclust(dist(t(trainSpam[,1:4])))
+plot(hC)
+hC=hclust(dist(t(log10(trainSpam[,1:55]+1))))
+plot(hC)
+
